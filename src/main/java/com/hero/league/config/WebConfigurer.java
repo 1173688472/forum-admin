e7201f473c94bf98c080d474478ec56c150c042f
shayu <shayu-yusha@qq.com>
2022-11-01 21:25:22 +0800

    删除security和添加WebConfig

---
+++ src/main/java/com/hero/league/config/WebConfigurer.java
@@ -0,0 +1,43 @@
+package com.hero.league.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: shayu
+ * @date: 2022/11/01
+ * @ClassName: forum-admin
+ * @Description:
+ */
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // addPathPatterns("/**") 表示拦截所有的请求，
+        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/user/login", "/user/register", "/user/list","/user/getData");
+        WebMvcConfigurer.super.addInterceptors(registry);    //较新Spring Boot的版本中这里可以直接去掉，否则会报错
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
